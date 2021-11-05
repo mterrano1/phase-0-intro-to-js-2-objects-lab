db8fd9c6cb92e5a456b50c0795ff9e76a31a02d2
Christopher Terrano <mterrano1@yahoo.com>
2021-11-05 15:02:28 -0500

    commit tree 737eb9ae6c373fb6157ba6347d54395a819caeb2

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const employee = {
-    name: "mike",
-    streetAddress: "123 Elm st",
+    name: "Bob",
+    streetAddress: "123 Elm st"
 }
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployee = {...employee};
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newEmployee = {...object};
 
     newEmployee[key] = value;
-
+    
     return newEmployee;
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-
-    return employee;
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    
+    return object;
 }
 
-function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = {...employee};
+function deleteFromEmployeeByKey(object, key) {
+    const newEmployeeTwo = {...object};
 
-    return {
-        newEmployee,
-    };
+    delete newEmployeeTwo[key];
+
+    return newEmployeeTwo;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, name, value) {
-    const newEmployee = employee;
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
 
-    newEmployee[name] = value;
-
-    return newEmployee;
+    return object;
 }
